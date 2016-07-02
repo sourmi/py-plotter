7adb68ed4c8dd2163d11bd0766f7cedd000e7c77
Sour Mi <sourmi@github.com>
2016-07-02 16:54:04 +0200

    more tests

--- test/plotter_test.py
+++ test/plotter_test.py
@@ -41,9 +41,45 @@ class TestPlotter(unittest.TestCase):
     motorX = TestMotor("x", moves)
     motorY = TestMotor("y", moves)
     plotter = plotter.Plotter(motorX, motorY)
+
+
+    def test_threePartsSameResultAsAllInOne(self):
+        self.reset()
+        self.plotter.lineTo(60,30)
+        moves_aio = self.moves.moves
+        self.reset()
+        self.plotter.lineTo(20,10)
+        self.plotter.lineTo(40,20)
+        self.plotter.lineTo(60,30)
+        moves_parts = self.moves.moves
+        self.assertEqual(moves_aio, moves_parts)
+
+    def test_twoPartsSameResultAsAllInOne(self):
+        self.reset()
+        self.plotter.lineTo(4,4)
+        self.assertEqual(self.moves.moves, "XYXYXYXY")
+        self.reset()
+        self.plotter.lineTo(2,2)
+        self.plotter.lineTo(4,4)
+        self.assertEqual(self.moves.moves, "XYXYXYXY")
+
     
+    def test_square(self):
+        self.reset()
+        self.plotter.lineTo(4,0)
+        self.plotter.lineTo(4,4)
+        self.plotter.lineTo(0,4)
+        self.plotter.lineTo(0,0)
+        self.assertPos(0,0)
+        self.assertEqual(self.moves.moves, "XXXXYYYYxxxxyyyy")
+        
+        
 
     def test_lineTo(self):
+        self.assertMove(-1,-1, "xy")
+        self.assertMove( 1,-1, "Xy")
+        self.assertMove(-1, 1, "xY")
+        self.assertMove( 1, 1, "XY")
         self.assertMove(-4,-2, "xyxxyx")
         self.assertMove(-4, 2, "xYxxYx")
         self.assertMove( 4,-2, "XyXXyX")
